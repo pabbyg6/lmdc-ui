2bcd9d1f86cd8284e989cabc9771acc140103171
Pablo Grant <grantpablo4@gmail.com>
2022-08-08 13:46:20 -0500

    wip: fix border collapse on table, export icon button

--- projects/components/src/public-api.ts
+++ projects/components/src/public-api.ts
@@ -8,3 +8,4 @@ export * from './lib/button/public_api';
 export * from './lib/table/public_api';
 export * from './lib/lmdc-card/lmdc-card';
 export * from './lib/icon/public_api';
+export * from './lib/icon-button/lmdc-icon-button';
